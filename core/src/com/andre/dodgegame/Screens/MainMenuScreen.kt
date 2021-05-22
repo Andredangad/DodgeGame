1915d2bfeefc81893f4078ac607b679d64b1d9d1
Andredangad <47104331+Andredangad@users.noreply.github.com>
2021-05-22 21:24:41 +0200

    add menu

--- core/src/com/andre/dodgegame/Screens/MainMenuScreen.kt
+++ core/src/com/andre/dodgegame/Screens/MainMenuScreen.kt
@@ -2,16 +2,32 @@ package com.andre.dodgegame.Screens
 
 import com.andre.dodgegame.DodgeGame
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 
 class MainMenuScreen(private val game: DodgeGame) : Screen {
-    private var playButtonActive:Texture = Texture(Gdx.files.internal("image/play_button_active.jpg"))
-    private var playButtonInactive:Texture = Texture(Gdx.files.internal("image/play_button_inactive.jpg"))
-    private var exitButtonActive:Texture = Texture(Gdx.files.internal("image/exit_button_active.jpg"))
-    private var exitButtonInactive:Texture = Texture(Gdx.files.internal("image/exit_button_inactive.jpg"))
 
+    private var playButtonInactive:Texture = Texture(Gdx.files.internal("image/play_button_inactive.png"))
+    private val PLAY_BUTTON_WIDTH = 300f
+    private val PLAY_BUTTON_HEIGHT = 120f
+
+    init {
+        Gdx.input.inputProcessor = object : InputAdapter() {
+            override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+
+                val x = game.camera.viewportWidth/2 - PLAY_BUTTON_WIDTH/2
+                val y = game.camera.viewportHeight - screenY
+                if(screenX > x && screenX < x + PLAY_BUTTON_WIDTH && y < game.camera.viewportHeight/2 + PLAY_BUTTON_HEIGHT/2 && y > game.camera.viewportHeight/2 - PLAY_BUTTON_HEIGHT/2  ){
+                    Gdx.app.log("dsq", screenX.toString())
+                    dispose()
+                    game.screen = MainGameScreen(game)
+                }
+                return super.touchUp(screenX, screenY, pointer, button)
+            }
+        }
+    }
     override fun hide() {
 
     }
@@ -24,7 +40,7 @@ class MainMenuScreen(private val game: DodgeGame) : Screen {
         Gdx.gl.glClearColor(1f,0f,0f,1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         game.batch.begin()
-        game.batch.draw(exitButtonActive)
+        game.batch.draw(playButtonInactive, game.camera.viewportWidth/2 - PLAY_BUTTON_WIDTH/2, game.camera.viewportHeight/2 - PLAY_BUTTON_HEIGHT/2, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT)
         game.batch.end()
     }
 
@@ -41,6 +57,6 @@ class MainMenuScreen(private val game: DodgeGame) : Screen {
     }
 
     override fun dispose() {
-
+        Gdx.input.inputProcessor = null
     }
 }
